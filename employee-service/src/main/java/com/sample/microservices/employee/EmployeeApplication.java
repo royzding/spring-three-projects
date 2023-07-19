b585c728a5b74801bccb63f5865c220bbd650d4c
Roy Ding <royzding@gmail.com>
2023-07-19 09:42:48 -0400

    discovery, api-gateway, employee-service, and swagger impl.

--- employee-service/src/main/java/com/sample/microservices/employee/EmployeeApplication.java
+++ employee-service/src/main/java/com/sample/microservices/employee/EmployeeApplication.java
@@ -3,7 +3,26 @@ package com.sample.microservices.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		servers = { 
+			@Server(url = "http://localhost:8082/employee"),
+			@Server(url = "http://localhost:8083/employee") 
+		}, 
+		info = @Info(title = "employee-service", 
+		version = "v3", 
+		description = "Employee-Service APIs", 
+		license = @License(name = "Apache 2.0", url = "http://foo.bar"), 
+		contact = @Contact(url = "http://employee-server.com", 
+		name = "Roy", 
+		email = "Roy@example.com"))
+)
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
