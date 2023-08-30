6e9d4cef3404c22bb0dfa7faa2f9145e0eb3b85f
Roy Ding <royzding@gmail.com>
2023-08-30 17:51:48 -0400

    added config server

--- common-ware/src/main/resources/files/schema/oracle_db_schema.sql
+++ common-ware/src/main/resources/files/schema/oracle_db_schema.sql
@@ -7,5 +7,9 @@ CREATE TABLE holiday_date
 	name 	VARCHAR2(50 CHAR), 
 	email 	VARCHAR2(50 CHAR), 
 	branch 	VARCHAR2(50 CHAR), 
+	active  SMALLINT,   ----- 1 or 0
 	PRIMARY KEY (id)
 );
+
+
+CREATE UNIQUE INDEX ON HOLIDAY_DATE (CASE WHEN ACTIVE = 1 THEN NAME || '.' || EMAIL END);
