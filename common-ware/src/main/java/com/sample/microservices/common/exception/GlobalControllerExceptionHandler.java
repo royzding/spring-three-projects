3c9e81346e7242ee218b055db1d956141eb35bb4
Roy Ding <royzding@gmail.com>
2024-11-14 11:39:47 -0500

    added response status.

--- common-ware/src/main/java/com/sample/microservices/common/exception/GlobalControllerExceptionHandler.java
+++ common-ware/src/main/java/com/sample/microservices/common/exception/GlobalControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -18,6 +19,7 @@ class GlobalControllerExceptionHandler {
   private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
   
   @ExceptionHandler(NotFoundException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
   public HttpErrorInfo globalResourceNotFoundException(WebRequest request, NotFoundException ex) {
     
     return new HttpErrorInfo(HttpStatus.NOT_FOUND, request.getDescription(false), ex.getMessage());
@@ -25,6 +27,7 @@ class GlobalControllerExceptionHandler {
   }  
 
   @ExceptionHandler(ValidationFailureException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
   public HttpErrorInfo globalglobalalidationFailureException(WebRequest request, ValidationFailureException ex) {
     
     return new HttpErrorInfo(HttpStatus.BAD_REQUEST, request.getDescription(false), ex.getMessage());
@@ -32,6 +35,7 @@ class GlobalControllerExceptionHandler {
   }    
   
   @ExceptionHandler(InternalRuntimeException.class)
+  //@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   public HttpErrorInfo globalInternalRuntimeException(WebRequest request, InternalRuntimeException ex) {
     
     return new HttpErrorInfo(HttpStatus.INTERNAL_SERVER_ERROR, request.getDescription(false), ex.getMessage());
